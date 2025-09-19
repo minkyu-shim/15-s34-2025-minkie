63303211c135e398e57800723d48a7312dbe2a35
Minkyu Shim <minkyu.shim@epita.fr>
2025-09-19 15:37:21 +0200

    submit project01

---
+++ project01/script.js
@@ -0,0 +1,16 @@
+// select the toggle button
+const toggleBtn = document.querySelector(".header__toggle-btn")
+// select the header
+const header = document.querySelector(".header")
+
+// toggleBtn.addEventListener("click", () => {
+// 	// add or remove the "header-open" class from the header element
+// 	// write your code here
+// 	header.classList.toggle("header-open");
+
+//     console.log("Toggled header-open class!");
+// })
+
+toggleBtn.addEventListener("click", () => {
+  header.classList.toggle("header-open");
+});
